daa0ef20aeda6741d3054a527d7fd614c84d28be
Pavan Boorla <linkmedeeps@gmail.com>
2017-05-17 17:35:37 +0530

    Final code.
    This code must work.

--- structure_array.c
+++ structure_array.c
@@ -14,11 +14,9 @@ typedef struct stu_s{
 
 
 void get_data(int *count, void *data){
-    
     stu_t *stu_data;
     stu_data = (stu_t*) malloc(sizeof(stu_t) * 10);
-    
-    stu_data = data;
+
     stu_data[0].roll_no = 12;
     stu_data[0].status = 0;
     stu_data[0].ratings = 90;
@@ -27,9 +25,8 @@ void get_data(int *count, void *data){
     stu_data[1].status = 0;
     stu_data[1].ratings = 92;
 
+    *((stu_t **)data) = stu_data;
     *count = 2;
-
-
 }
 
 int main(){
@@ -37,12 +34,11 @@ int main(){
     int count = 0 , i = 0;
     stu_t *stu_p;
 
-    get_data(&count, (void*) stu_p);
+    get_data(&count, &stu_p);
 
     for(i = 0; i < count; i ++){
-
         printf("%d %d %d\n", stu_p[i].roll_no,  stu_p[i].status, stu_p[i].ratings);
     }
 
-return 0;
+    return 0;
 }
